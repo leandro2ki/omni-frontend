9157bc3fcc707458ad4c8ea1afc064b5b96547b9
Leandro Kimura <leandro2ki@gmail.com>
2019-05-02 20:13:42 -0300

    update experiment id

--- src/AppTestAB.js
+++ src/AppTestAB.js
@@ -4,7 +4,7 @@ import { Experiment, Variant } from "react-optimize";
 class AppTestAB extends React.Component {
   render() {
     return (
-      <Experiment id="AB">
+      <Experiment id="hCn01RZ1S7yfYQS2U36b-A">
         <Variant id="0">Original</Variant>
         <Variant id="1">Variant 1</Variant>
         <Variant id="2">Variant 2</Variant>
